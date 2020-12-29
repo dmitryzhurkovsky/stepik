7f8ecc31e51a271d4bfa59e5f6a413cfe2abe7af
DmitryZhurkovsky <62201197+dmitryzhurkovsky@users.noreply.github.com>
2020-12-29 09:37:18 +0300

    Delete nodule1.py

--- nodule1.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-with open('/Users/dmitry/downloads/dataset_3378_2.txt', 'r') as infile:
-    url = infile.read().strip()
-    text = requests.get(url).text.splitlines()
-    print(len(text))
